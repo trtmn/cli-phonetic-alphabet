46f41c084fcac6f6faa4e78ae75e59ee56726d00
Matt Troutman <git@trtmn.com>
2025-04-07 10:41:41 -0500

    Fix dependencies in Homebrew formula

--- cli-phonetic-alphabet.rb
+++ cli-phonetic-alphabet.rb
@@ -4,12 +4,15 @@ class CliPhoneticAlphabet < Formula
   desc "A CLI version of the NATO Phonetic alphabet"
   homepage "https://github.com/trtmn/cli-phonetic-alphabet"
   url "https://github.com/trtmn/cli-phonetic-alphabet/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "6bd4e21e78dfe5ec9cebe2e690530acc12c22f5b86a62b8f934e8dd1f8bfe6ce"
+  sha256 "9e5764d427e9e91847597458e4a3a85b177c8b45bba5d5b3946bd3feac31a2d1"
 
   depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
+    
+    # Explicitly install dependencies
+    system Formula["python@3.11"].opt_bin/"pip3", "install", "click>=8.0.0", "rich>=10.0.0"
   end
 
   test do
