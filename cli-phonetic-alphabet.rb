d7bf9f61e8b2d6d86e006628f54bfe3e78af9ea2
Matt Troutman <git@trtmn.com>
2025-04-06 22:29:36 -0500

    Bump version to 0.1.1

--- cli-phonetic-alphabet.rb
+++ cli-phonetic-alphabet.rb
@@ -3,8 +3,8 @@ class CliPhoneticAlphabet < Formula
 
   desc "A CLI version of the NATO Phonetic alphabet"
   homepage "https://github.com/trtmn/cli-phonetic-alphabet"
-  url "https://github.com/trtmn/cli-phonetic-alphabet/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "34374de4fb17a9b8392e98f48907183e725eea1b420d0a87674214cc28f0c077"
+  url "https://github.com/trtmn/cli-phonetic-alphabet/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   depends_on "python@3.11"
 
