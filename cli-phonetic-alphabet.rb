7c090087bf21a743530d8d9ce8a437493a1aa7d2
Matt Troutman <git@trtmn.com>
2025-04-06 22:29:09 -0500

    Update Python version requirement to >=3.11

--- cli-phonetic-alphabet.rb
+++ cli-phonetic-alphabet.rb
@@ -2,11 +2,11 @@ class CliPhoneticAlphabet < Formula
   include Language::Python::Virtualenv
 
   desc "A CLI version of the NATO Phonetic alphabet"
-  homepage "https://github.com/yourusername/cli-phonetic-alphabet"
-  url "https://github.com/yourusername/cli-phonetic-alphabet/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "YOUR_SHA256_HASH"  # You'll need to replace this with the actual SHA256 hash
+  homepage "https://github.com/trtmn/cli-phonetic-alphabet"
+  url "https://github.com/trtmn/cli-phonetic-alphabet/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "34374de4fb17a9b8392e98f48907183e725eea1b420d0a87674214cc28f0c077"
 
-  depends_on "python@3.8"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
